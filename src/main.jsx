4670a5b73b3f04061356a3c43f438b23d3534742
alex-zander-0403 <alexzander0403@gmail.com>
2026-03-18 23:54:20 +0300

    add base reducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { legacy_createStore as createStore } from "redux";
 
-createRoot(document.getElementById('root')).render(
+// ============================================================
+
+const defaultState = {
+  cash: 0,
+};
+
+// const action = {
+//   type: "ADD_CASH",
+//   payload: 100
+// }
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    //
+    case "ADD_CASH":
+      return { ...state, cash: state.cash + action.payload };
+
+    case "GET_CASH":
+      return { ...state, cash: state.cash - action.payload };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+// ============================================================
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
-)
+);
