1ae570dd49ac6c18e05f1781584234199e35845c
alex-zander-0403 <alexzander0403@gmail.com>
2026-03-21 19:15:18 +0300

    add case ADD_CUSTOMER in customerReducer

--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -5,11 +5,17 @@ const defaultState = {
 export function customerReducer(state = defaultState, action) {
   switch (action.type) {
     //
-    case "ADD_CUSTOMER":
-      return { ...state, cash: state.cash + action.payload };
+    // case "ADD_CUSTOMER":
+    //   return { ...state, cash: state.cash + action.payload };
 
-    case "GET_CUSTOMERS":
-      return { ...state, cash: state.cash - action.payload };
+    // case "GET_CUSTOMERS":
+    //   return { ...state, cash: state.cash - action.payload };
+
+    case "ADD_CUSTOMER":
+      return { ...state, customers: [...state.customers, action.payload] };
+
+    // case "REMOVE_CUSTOMERS":
+    //   return state;
 
     default:
       return state;
